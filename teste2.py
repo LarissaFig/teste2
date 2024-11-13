08d99b9940f2d612718da8e33a6e949f254be47e
LarissaFig <larissams2122@gmail.com>
2024-11-13 11:40:55 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -12,3 +12,7 @@ st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 st.slider('Qual o grau de satisfação?', 0, 100)
 x = st.select_slider('Qual o grau de satisfação?', [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
 st.write(x)
+
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.color_picker('Escolha sua cor favorita')

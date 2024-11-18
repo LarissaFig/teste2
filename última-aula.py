3f2407fb1e560bc208732423a46ec297a1c2324f
LarissaFig <larissams2122@gmail.com>
2024-11-18 11:41:16 -0300

    última-aula.py

--- última-aula.py
+++ última-aula.py
@@ -2,27 +2,24 @@ import streamlit as st
 import pandas as pd
 import requests as rqt
 dadosMulheres = rqt.get('https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome')
-dadosMulheres
+
 dadosMulheres.headers['Content-Type']
 dados_M_json = dadosMulheres.json()
 
-type(dados_M_json)
+
 dados_M_json.keys()
 dfmulheres = pd.DataFrame(dados_M_json['dados'])
-dfmulheres
 
 dfmulheres['sexo'] = 'feminino'
-dfmulheres.info()
 
 dadosHomens = rqt.get('https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome')
-dadosHomens
 
 dados_H_json = dadosHomens.json()
 dfhomens = pd.DataFrame(dados_H_json['dados'])
-dfhomens
+
 dfhomens['sexo'] = 'masculino'
 df_concat = pd.concat([dfmulheres, dfhomens], axis = 'index')
-df_concat
+
 #Crie um dashboard no streamlit com essas informações e insira um selectbox que realize filtragem por gênero.
 st.title ('Análise da quantidade de deputados por sexo')
 sexo_selecionado = st.selectbox('Selecione o sexo', df_concat['sexo'].unique())
@@ -31,7 +28,7 @@ sexo_selecionado = st.selectbox('Selecione o sexo', df_concat['sexo'].unique())
 # Se necessário, peça ajuda ao chat GPT sobre como calcular a quantidade de deputados por estado.
 st.header('Quantidade de deputados por estado', sexo_selecionado)
 dadosFiltrados = df_concat[df_concat['sexo'] == sexo_selecionado]
-st.title('Deputados do sexo' + sexo_selecionado)
+st.title('Deputados do sexo ' + sexo_selecionado)
 ocorrencias = dadosFiltrados['siglaUf'].value_counts()
 dfEstados = pd.DataFrame({
       'siglaUf':ocorrencias.index,
@@ -46,7 +43,7 @@ st.dataframe(dadosFiltrados)
 
 #Acrescente informações sobre o total de deputados, além do total de homens e mulheres.
 totalHomens = dfhomens['id'].count()
-st.metric('O tootal de homens é ', totalHomens)
+st.metric('O total de homens é ', totalHomens)
 
 totalMulheres = dfmulheres['id'].count()
 st.metric('O total de mulheres é ', totalMulheres)
